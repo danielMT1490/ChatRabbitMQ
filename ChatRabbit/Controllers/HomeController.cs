040d199f81e4089539fdf47e83898d0ccec0393c
Daniel Madrigal <danieltristan1991@gmail.com>
2018-05-17 13:11:03 +0200

    chatrabbit

---
+++ ChatRabbit/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using ChatRabbit.Models;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace ChatRabbit.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataLayer dl = new DataLayer();
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (Session["userid"] == null)
+            {
+                return RedirectToAction("login");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        public JsonResult sendmsg(string message, string friend)
+        {
+            RabbitMQBII obj = new RabbitMQBII();
+            IConnection con = obj.GetConnection();
+            bool flag = obj.send(con, message, friend);
+            return Json(null);
+        }
+        [HttpPost]
+        public JsonResult receive()
+        {
+            try
+            {
+                RabbitMQBII obj = new RabbitMQBII();
+                IConnection con = obj.GetConnection();
+                string userqueue = Session["username"].ToString();
+                string message = obj.receive(con, userqueue);
+                return Json(message);
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+
+
+        }
+        public ActionResult login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(FormCollection fc)
+        {
+            string email = fc["txtemail"].ToString();
+            string password = fc["txtpassword"].ToString();
+            UserModel user = dl.Login(email, password);
+            if (user.userid > 0)
+            {
+                ViewData["status"] = 1;
+                ViewData["msg"] = "login Successful...";
+                Session["username"] = user.email;
+                Session["userid"] = user.userid.ToString();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                ViewData["status"] = 2;
+                ViewData["msg"] = "invalid Email or Password...";
+                return View();
+            }
+
+        }
+
+        [HttpPost]
+        public JsonResult friendlist()
+        {
+            int id = Convert.ToInt32(Session["userid"].ToString());
+            List<UserModel> users = dl.Getusers(id);
+            List<ListItem> userlist = new List<ListItem>();
+            foreach (var item in users)
+            {
+                userlist.Add(new ListItem
+                {
+                    Value = item.email.ToString(),
+                    Text = item.email.ToString()
+
+                });
+            }
+            return Json(userlist);
+        }
+
+    }
+}
---
+++ ChatRabbit/Models/DataLayer.cs
@@ -0,0 +1,71 @@
+﻿using ChatRabbit.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ChatRabbit.Models
+{
+    public class DataLayer
+    {
+            
+        public UserModel Login(string email, string password)
+        {
+            UserModel mindundi = new UserModel
+            {
+                userid=1,
+                email="mindundi@gmail.com",
+                mobile="56894875",
+                password="mindundi",
+                confirmpassword="mindundi",
+                dob="14/12/1990"
+            };
+            UserModel piltrafa = new UserModel
+            {
+                userid = 1,
+                email = "piltrafa@gmail.com",
+                mobile = "56894875",
+                password = "piltrafa",
+                confirmpassword = "mindundi",
+                dob = "14/12/1990"
+            };
+            if (password.Equals(mindundi.password))
+            {
+                return mindundi;
+            }
+            else
+            {
+                return piltrafa;
+            }
+            
+        }
+
+        public List<UserModel> Getusers(int id)
+        {
+            List<UserModel> userlist = new List<UserModel>
+            {
+                new UserModel
+                {
+                 userid=1,
+                email="mindundi@gmail.com",
+                mobile="56894875",
+                password="mindundi",
+                confirmpassword="mindundi",
+                dob="14/12/1990"
+                },
+                new UserModel
+                {
+                 userid=1,
+                email="piltrafa@gmail.com",
+                mobile="56894875",
+                password="piltrafa",
+                confirmpassword="piltrafa",
+                dob="14/12/1990"
+                },
+            };
+           
+            return userlist;
+        }
+    }
+}
